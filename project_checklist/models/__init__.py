e8c901adf255baa87d2451103724e6939e0af8dc
bhadawi404 <abadawi.911@gmail.com>
2025-11-11 23:02:00 +0700

    fixing error

--- project_checklist/models/__init__.py
+++ project_checklist/models/__init__.py
@@ -2,5 +2,5 @@ from . import project_checklist
 from . import project_checklist_line
 from . import project_checklist_rule
 from . import project_checklist_rule_action
-from . import checklist_views
 from . import project_page
+from . import checklist_views
