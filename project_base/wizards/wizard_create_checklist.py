20ea58fc540e5d3663fa9497b9d91ec4e8af51c3
bhadawi404 <abadawi.911@gmail.com>
2025-11-17 23:03:30 +0700

    fixing checklist

--- project_base/wizards/wizard_create_checklist.py
+++ project_base/wizards/wizard_create_checklist.py
@@ -3,26 +3,32 @@ from odoo.exceptions import UserError, ValidationError
 
 class BulkCreateTaskChecklistWizard(models.TransientModel):
     _name = 'bulk.create.task.checklist'
-    _description = 'Create Task Checklist'
+    _description = 'Create Checklist'
 
     project_id = fields.Many2one('project.project', string='Project', required=True)
+    is_related_task = fields.Boolean('Add a Task ?')
+    task_id = fields.Many2one('project.task', string='Task')
     checklist_ids = fields.One2many('bulk.create.task.checklist.line', 'wizard_id', string='Checklist')
     
+    @api.onchange('is_related_task')
+    def _onchange_is_related_task(self):
+        if not self.is_related_task:
+            self.task_id = False
+
     def action_confirm(self):
         self.ensure_one()
         if not self.checklist_ids:
             raise ValidationError(_("Please input at least one checklist in the list."))
         
-        for line in self.checklist_ids:
-            if line.task_id and line.task_id.project_id != self.project_id:
-                raise ValidationError(_(
-                    "Task '%s' does not belong to the selected project '%s'."
-                ) % (line.task_id.name, self.project_id.name))
+        if self.is_related_task and self.task_id.project_id != self.project_id:
+            raise ValidationError(_(
+                "Task '%s' does not belong to the selected project '%s'."
+            ) % (self.task_id.name, self.project_id.name))
 
         for line in self.checklist_ids:
             task_checklist = self.env['project.task.checklist'].create({
                 'name': line.name,
-                'task_id': line.task_id.id,
+                'task_id': self.task_id.id if self.task_id else False,
                 'project_id': self.project_id.id,
                 'description': line.description,
                 'status': 'todo',
@@ -36,7 +42,6 @@ class BulkCreateTaskChecklistLineWizard(models.TransientModel):
     _description = 'Create Tasks Checklist Line'
 
     wizard_id = fields.Many2one('bulk.create.task.checklist', string='Wizard')
-    task_id = fields.Many2one('project.task', string='Task')
     name = fields.Char('Title')
     user_ids = fields.Many2many('res.users', string='Assignees')
     description = fields.Text('Description')
